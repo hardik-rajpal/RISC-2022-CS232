914c20c0835205d9bf337c7424dd75185e971eaa
Hardik <hardikrajpal@cse.iitb.ac.in>
2022-04-16 16:34:44 +0530

    ...

--- qfiles/ALU.vhd
+++ qfiles/ALU.vhd
@@ -5,22 +5,13 @@ use IEEE.numeric_std.all;
 entity ALU is
     port (a,b: in std_logic_vector(15 downto 0) ;
         ctrl: in std_logic_vector(2 downto 0) ; 
-        clk: in std_logic;
         c: out std_logic_vector(15 downto 0);
         cout: out std_logic) ;
 end ALU;
 
 architecture Behavioral of ALU is
-
-begin
-    process (clk,a,b)
-    begin
-        report "inputs: "&integer'image(to_integer(unsigned(a)))&", "&integer'image(to_integer(unsigned(b)));
-        if rising_edge(clk) then
-            if(ctrl="000") then
-                c<= a+b;
+    if(ctrl="000") then
+        c<= a+b;
 --                report "output:"&integer'image(to_integer(unsigned(c)));
-            end if;
-        end if;
-    end process;
+    end if;
 end Behavioral;
